6a141837d8d00bd183b513e2bb5fd07d3f143607
Kuchita El <el.kuchita@gmail.com>
2024-02-12 20:06:57 +0900

    Nuxt module に変換

---
+++ playground/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <feature-toggle feature="test">
+      <template #default>test</template>
+      <template #fallback>old test</template>
+    </feature-toggle>
+  </div>
+</template>
--- src/module.ts
+++ src/module.ts
@@ -4,11 +4,11 @@ import {addImports} from "nuxt/kit";
 export default defineNuxtModule({
     setup() {
         const {resolve} = createResolver(import.meta.url)
-        addComponent({name: "FeatureToggle", filePath: resolve("src/runtime/components/feature-toggle.vue")})
+        addComponent({name: "FeatureToggle", filePath: resolve("runtime/components/feature-toggle.vue")})
 
         addImports({
             name: "useFeatureToggle",
-            from: resolve("src/runtime/composables/feature-toggle.ts")
+            from: resolve("runtime/composables/feature-toggle.ts")
         })
     }
 })
